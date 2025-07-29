4473a53fd4da52ed393be4813959cfda856ca65f
Nejc Drobnic <nejc@flashnet.xyz>
2025-07-29 16:49:50 +0200

    add posthog

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,24 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
+
+	async rewrites() {
+		return [
+			{
+				source: "/relay-znVB/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/relay-znVB/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+			{
+				source: "/relay-znVB/flags",
+				destination: "https://us.i.posthog.com/flags",
+			},
+		];
+	},
+	skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
